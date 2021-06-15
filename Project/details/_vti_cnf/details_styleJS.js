3049982a0aecf8cb60ecaf9296458dc2d740d091
Freya <64510536+FreyaKim0@users.noreply.github.com>
2021-06-14 21:21:48 -0400

    initial commit

---
+++ Project/details/_vti_cnf/details_styleJS.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-FOG0J7P\\Freya
+vti_modifiedby:SR|DESKTOP-FOG0J7P\\Freya
+vti_timelastmodified:TR|17 Apr 2020 19:21:41 -0000
+vti_timecreated:TR|16 Apr 2020 00:50:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Project/details/details.html
+vti_nexttolasttimemodified:TW|17 Apr 2020 19:09:41 -0000
+vti_cacheddtm:TX|17 Apr 2020 19:21:41 -0000
+vti_filesize:IR|2269
